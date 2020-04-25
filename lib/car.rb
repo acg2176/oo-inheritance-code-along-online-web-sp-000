16513cd4cff0e122a520c16ed649c799529c7235
Anna Guerrero <acg2176@columbia.edu>
2020-04-25 20:56:06 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -3,9 +3,9 @@ require_relative "./vehicle.rb"
 class Car
   attr_accessor :wheel_size, :wheel_number
 
-  def initialize(size, number)
-    @size = size
-    @number = number
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
   end
 
   # def wheel_size

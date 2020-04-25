905be86aa823cd4cee889c7299ec5d83ec22031a
Anna Guerrero <acg2176@columbia.edu>
2020-04-25 20:54:34 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,23 @@
 require_relative "./vehicle.rb"
+
+class Car
+
+  def initialize(size, number)
+    @size = size
+    @number = number
+  end
+
+  def wheel_size
+    @size
+  end
+
+  def wheel_number
+    @number
+  end
+
+  def go
+    'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+  end
+
+
+end
